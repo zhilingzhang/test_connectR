49f3f056ea6cbe38e28349c0eb54d2a0a34af738
zhiling <zhilingzhang@wustl.edu>
2021-02-01 19:34:50 -0600

    version1

--- 1.R
+++ 1.R
@@ -2,4 +2,4 @@ packages <- c('ISLR', 'caret', 'tidyverse', 'ggthemes', 'rpart', 'rpart.plot',
               'knitr', 'kableExtra')
 sapply(packages, require, character.only = TRUE)
 data(OJ)
-set.seed(2)
+set.seed(5)
